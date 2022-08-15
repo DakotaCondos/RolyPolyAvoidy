d783dbe9ff0aff02f94729dfe75b07bb174a8930
mainPC <dakota242424@live.com>
2022-08-14 19:27:02 -0700

    Basic WASD Movement added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float xVelocity = 0.0f;
+    [SerializeField] float yVelocity = 0.0f;
+    [SerializeField] float zVelocity = 0.0f;
+    [SerializeField] float moveSpeed = 0.1f;
+
+    Vector2 rawInput;
+
+
     void Start()
     {
+
+    }
+
+    void Update()
+    {
+        Move();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
+        Vector2 delta = rawInput * Time.deltaTime * moveSpeed;
+        Vector3 newPos = new Vector3(transform.position.x + delta.x, transform.position.y, transform.position.z + delta.y);
+        transform.position = newPos;
         
+    }
+
+    private void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
     }
 }
